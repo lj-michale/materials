6630ac0a9b48350414032a7e9bf98b6f93d0c748
ircloud <realtimecompute.luojie@gmail.com>
2021-06-06 17:33:35 +0800

    feat: 数据结构与算法知识整理-堆【Heap】篇

--- 语言/Java/java-learning/src/main/java/com/example/leetcode/heap/Heap.java
+++ 语言/Java/java-learning/src/main/java/com/example/leetcode/heap/Heap.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /**
  * @description
- *
+ * Java类集中的ArrayList实现Heap
  * @author lj.michale
  * @date 2021-06-06
  */
@@ -26,8 +26,8 @@ public class Heap {
     }
 
     public int deletRoot(){//删除根节点并返回其值
-        int mid_root=this.arryList.get(0);
-        this.mid_i=this.arryList.get(this.arryList.size()-1);
+        int mid_root = this.arryList.get(0);
+        this.mid_i = this.arryList.get(this.arryList.size()-1);
         this.arryList.remove(this.arryList.size()-1);
         this.arryList.set(0,this.mid_i);
         shiftDown(0);
@@ -38,31 +38,31 @@ public class Heap {
         if(index<0 || index>this.arryList.size()-1) {
             throw new IndexOutOfBoundsException("删除节点index范围有误");
         }
-        int mid_value=this.arryList.get(index);
+        int mid_value = this.arryList.get(index);
         this.mid_i=this.arryList.get(this.arryList.size()-1);
         this.arryList.remove(this.arryList.size()-1);
-        this.arryList.set(index,this.mid_i);
+        this.arryList.set(index, this.mid_i);
         shiftDown(index);
         return mid_value;
     }
 
     private void shiftUp(int index){//添加数据的时候进行操作
-        if(type){//最大堆
-            if((index-1)/2 != -1){
+        if(type) {//最大堆
+            if((index-1) / 2 != -1) {
                 if(this.arryList.get((index - 1) / 2) <this.arryList.get(index)){
-                    mid_i=this.arryList.get((index - 1) / 2);
+                    mid_i = this.arryList.get((index - 1) / 2);
                     this.arryList.set((index - 1) / 2,this.arryList.get(index));
                     this.arryList.set(index,mid_i);
                     //递归调用
                     shiftUp((index - 1) / 2);
                 }
             }
-        }else {//最小堆
-            if((index-1)/2!=-1){
+        } else {//最小堆
+            if((index-1)/2 != -1) {
                 if(this.arryList.get((index - 1) / 2) >this.arryList.get(index)){
-                    mid_i=this.arryList.get((index - 1) / 2);
+                    mid_i = this.arryList.get((index - 1) / 2);
                     this.arryList.set((index - 1) / 2,this.arryList.get(index));
-                    this.arryList.set(index,mid_i);
+                    this.arryList.set(index, mid_i);
                     //递归调用
                     shiftUp((index - 1) / 2);
                 }
@@ -72,23 +72,23 @@ public class Heap {
 
     private void shiftDown(int index){//删除数据的时候进行操作
         if(type){//最大堆
-            if(index*2+1 < this.arryList.size()){
-                if(this.arryList.get(2*index+1) >this.arryList.get(index)){
-                    mid_i=this.arryList.get(2*index+1);
-                    this.arryList.set(2*index+1,this.arryList.get(index));
-                    this.arryList.set(index,mid_i);
+            if(index * 2 + 1 < this.arryList.size()) {
+                if(this.arryList.get(2 * index + 1) > this.arryList.get(index)){
+                    mid_i = this.arryList.get(2 * index+1);
+                    this.arryList.set(2 * index + 1, this.arryList.get(index));
+                    this.arryList.set(index, mid_i);
                     //递归调用
-                    shiftDown(2*index+1);
+                    shiftDown(2 * index + 1);
                 }
             }
-        }else {//最小堆
-            if(index*2+1 < this.arryList.size()){
-                if(this.arryList.get(2*index+1) <this.arryList.get(index)){
-                    mid_i=this.arryList.get(2*index+1);
-                    this.arryList.set(2*index+1,this.arryList.get(index));
-                    this.arryList.set(index,mid_i);
+        } else {//最小堆
+            if(index * 2 + 1 < this.arryList.size()) {
+                if(this.arryList.get(2 * index + 1) < this.arryList.get(index)){
+                    mid_i = this.arryList.get(2 * index + 1);
+                    this.arryList.set(2 * index + 1,this.arryList.get(index));
+                    this.arryList.set(index, mid_i);
                     //递归调用
-                    shiftDown(2*index+1);
+                    shiftDown(2 * index+1);
                 }
             }
         }
